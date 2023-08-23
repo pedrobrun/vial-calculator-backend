dcdaa4d916998eb9933b6a88f89da0196c37dbdf
Pedro Brun <pedroluccavb@gmail.com>
2023-08-22 22:32:07 -0300

    feat: add hashing to user password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
+  private hashSalt = 12;
 
   async create(createUserDto: CreateUserDto) {
-    return await this.userRepository.create(createUserDto);
+    const { password, username } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, this.hashSalt);
+    return await this.userRepository.create({
+      username,
+      password: hashedPassword,
+    });
   }
 }
