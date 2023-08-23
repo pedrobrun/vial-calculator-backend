87fca3b1e2eb4a54738ade4dcc95e3584ee4e60b
Pedro Brun <pedroluccavb@gmail.com>
2023-08-22 22:00:47 -0300

    chore: add config module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+
+  const { PORT } = process.env;
+  await app.listen(PORT);
+  Logger.log(`🚀 Running on ${PORT}`);
 }
 bootstrap();
