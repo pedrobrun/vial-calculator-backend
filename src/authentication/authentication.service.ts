28ee24968f468c29e0700652cc3eb4ebe406655a
Pedro Brun <pedroluccavb@gmail.com>
2023-08-24 11:46:53 -0300

    feat: improve signIn bad request message

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -16,7 +16,8 @@ export class AuthenticationService {
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.userService.findByUsername(username);
-    if (!user || !user.password) throw new BadRequestException();
+    if (!user || !user.password)
+      throw new BadRequestException('User does not exist.');
 
     if (!(await bcrypt.compare(pass, user.password))) {
       throw new UnauthorizedException();
