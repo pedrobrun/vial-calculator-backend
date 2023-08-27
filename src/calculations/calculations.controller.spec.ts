5e9ac1d3aefea5510b90a276140ad4603de74f1d
Pedro Brun <pedroluccavb@gmail.com>
2023-08-27 19:09:28 -0300

    test: fix dependencies

--- src/calculations/calculations.controller.spec.ts
+++ src/calculations/calculations.controller.spec.ts
@@ -1,14 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CalculationsController } from './calculations.controller';
 import { CalculationsService } from './calculations.service';
+import { CalculationsRepository } from './calculations.repository';
+import { getModelToken } from '@nestjs/mongoose';
+import { Caclulations } from './calculations.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('CalculationsController', () => {
   let controller: CalculationsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule],
       controllers: [CalculationsController],
-      providers: [CalculationsService],
+      providers: [
+        CalculationsService,
+        CalculationsRepository,
+        {
+          provide: getModelToken(Caclulations.name),
+          useValue: {
+            find: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<CalculationsController>(CalculationsController);
--- src/calculations/calculations.service.spec.ts
+++ src/calculations/calculations.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CalculationsService } from './calculations.service';
+import { CalculationsRepository } from './calculations.repository';
+import { Caclulations } from './calculations.schema';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('CalculationsService', () => {
   let service: CalculationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CalculationsService],
+      providers: [
+        CalculationsService,
+        CalculationsRepository,
+        {
+          provide: getModelToken(Caclulations.name),
+          useValue: {
+            find: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CalculationsService>(CalculationsService);
